a10b9c9fd339328bc6f5bba5af77ccdab1e8f72e
Gift Christian <140011591+gcl140@users.noreply.github.com>
2025-11-16 02:29:55 -0500

    db+google auth

--- dadjokes/settings.py
+++ dadjokes/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,16 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -150,9 +143,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -164,14 +154,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gftinity01@gmail.com'  # Use your Gmail address here
-# EMAIL_HOST_PASSWORD = 'pjrrcldgbkeiwapb'  # Use the app password (not your Google account password)
-EMAIL_HOST_PASSWORD = 'lwuhmvurprfjbdso'  # Use the app password (not your Google account password)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Use your Gmail address here
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use the app password (not your Google account password)
 
 
 # Google OAuth2 keys (now from environment)
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('GOOGLE_OAUTH2_KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('GOOGLE_OAUTH2_SECRET')
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_CLIENT_ID")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
@@ -201,16 +190,13 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
-    'forum.pipeline.prevent_duplicate_social_auth',  # ✅ Here
+    'dadjokes.pipeline.prevent_duplicate_social_auth',  # ✅ Here
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-
-
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'forum.pipeline.save_user_details',  # where you set is_parent, etc.
+    'dadjokes.pipeline.save_user_details',  # where you set is_parent, etc.
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
-
